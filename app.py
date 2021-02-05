90ba994785fb7bfe61ccb06011471aa334b8b3c2
ethanschreur <ethanschreur@icloud.com>
2021-02-05 16:32:39 -0500

    1 through 7

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route('/')
+def base():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('survey_start.html', title = title, instructions = instructions)
+
+@app.route('/questions/<int:number>')
+def questions(number):
+    if (len(responses) == len(satisfaction_survey.questions)):
+        stop_messing()
+        return redirect('/thanks')
+    if (not number == len(responses)):
+        stop_messing()
+        return redirect(f'/questions/{len(responses)}')
+    question = satisfaction_survey.questions[number]
+    question_text = question.question
+    question_answers = question.choices
+    return render_template('questions.html', question_text = question_text, question_answers = question_answers)
+
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    responses.append(request.form['answer'])
+    if (len(responses) < len(satisfaction_survey.questions)):
+        next_question_number = len(responses)
+        new_link = f'/questions/{str(next_question_number)}'
+        return redirect(new_link)
+    else:
+        return redirect('/thanks')
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+def stop_messing():
+    return flash('You\'re trying to access an invalid question')
